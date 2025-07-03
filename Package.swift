7e160ff2f4c2c22fdf275918f20cf74d3749a9ca
Dawinder Kaur <dawinder.kaur@cometchat.com>
2025-07-04 01:12:01 +0530

    v4.1.3

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CometChatCallsSDK",
-            url: "https://library.cometchat.io/ios/v4.0/xcode16/CometChatCallsSDK_4_1_2.xcframework.zip",
-            checksum: "2170924015380801b3fa679a7a87d02472f2eb59947547da283fe0c49828ea20"
+            url: "https://library.cometchat.io/ios/v4.0/xcode16/CometChatCallsSDK_4_1_3.xcframework.zip",
+            checksum: "1bb2884c9648834fe56f5aaf08979a9a0fd29de8129f55af0279e79b4543bcb8"
         ),
         .binaryTarget(
             name: "WebRTC",
